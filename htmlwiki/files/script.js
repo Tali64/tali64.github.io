7b56b0c5d59c733b70a261d5feed5acc4ba12773
Tali64 <63979637+Tali64@users.noreply.github.com>
2021-12-26 18:00:35 -0600

    Add files via upload

---
+++ htmlwiki/files/script.js
@@ -0,0 +1,60 @@
+window.onload = function () {
+    var x = document.getElementById("editor");
+    x.style.display = "none";
+    document.getElementById("editbox").value = document.getElementById("content").innerHTML;
+}
+function addSpecialText() {
+    var txt = document.getElementById("editbox").value;
+    var txtype = document.getElementById("texttype").innerHTML;
+    var addtxt = document.getElementById("specialtext").value;
+    var select = document.querySelector("#texttype");
+    var output = select.value;
+    if (output == "bold") {
+        document.getElementById("editbox").value = txt + "<b>" + addtxt + "</b>";
+    } else if (output == "italic") {
+        document.getElementById("editbox").value = txt + "<i>" + addtxt + "</i>";
+    } else if (output == "underline") {
+        document.getElementById("editbox").value = txt + "<u>" + addtxt + "</u>";
+    }
+}
+var support = (function() {
+    if (!window.DOMParser) return false;
+    var parser = new DOMParser();
+    try {
+        parser.parseFromString('x', 'text/html');
+    } catch(err) {
+        return false;
+    }
+    return true;
+})();
+var textToHTML= function(str) {
+    // check for DOMParser support
+    if (support) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(str, 'text/html');
+        return doc.body.innerHTML;
+    }
+
+    // Otherwise, create div and append HTML
+    var dom = document.createElement('div');
+    dom.innerHTML = str;
+    return dom;
+
+};
+function submit() {
+    var txt = document.getElementById("editbox").value;
+    document.getElementById("content").innerHTML = textToHTML(txt);
+    hideEditArea();
+}
+function hideEditArea() {
+    var x = document.getElementById("editor");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+}
+function showEditArea() {
+    var x = document.getElementById("editor");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+}
